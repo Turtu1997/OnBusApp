b20818e476d6c99c579c20468bdd5d2dc3188eba
Turtu1997 <turtu97@gmail.com>
2017-05-25 17:05:32 +0200

    Ciclo di vita Activity

--- app/src/main/java/com/example/stefanotortone/onbusapp/MainActivity.java
+++ app/src/main/java/com/example/stefanotortone/onbusapp/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.stefanotortone.onbusapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "ONBUSAPP:MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "----> onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "----> onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "----> onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "----> onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "----> onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "----> onDestroy");
     }
 }
