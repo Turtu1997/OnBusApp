ba38d29403ef8c2342716ef5fc20133f0f2fb396
Turtu1997 <turtu97@gmail.com>
2017-06-14 10:36:37 +0200

    i create the page cercafermata

---
+++ app/src/main/java/com/example/stefanotortone/onbusapp/CercaFermata.java
@@ -0,0 +1,63 @@
+package com.example.stefanotortone.onbusapp;
+
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by stefanotortone on 14/06/17.
+ */
+
+public class CercaFermata extends MainActivity {
+
+
+
+
+        final String TAG = "ONBUSAPP:CercaFermata";
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.cercafermata);
+            Log.i(TAG, "----> onCreate");
+        }
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.i(TAG, "----> onStart");
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.i(TAG, "----> onResume");
+        }
+
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.i(TAG, "----> onPause");
+        }
+
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.i(TAG, "----> onStop");
+        }
+
+        @Override
+        protected void onRestart() {
+            super.onRestart();
+            Log.i(TAG, "----> onRestart");
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.i(TAG, "----> onDestroy");
+        }
+
+
+}
